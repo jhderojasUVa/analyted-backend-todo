f134bac03037a1ce343e72aa2a627c88d9144185
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2022-08-12 12:43:35 +0100

    feat(): completed calls

--- src/index.js
+++ src/index.js
@@ -91,42 +91,50 @@ connection.on('connect', (err) => {
         connection.execSql(request);
     });
 
+    // Get one todo
     app.get('/:id', (req, res) => {
         // Initialice
         var rows = [];
         var rowCount = 0;
 
-        const request = new Request(`SELECT id, description, completed, date FROM Todo WHERE id='${req.params.id}' ORDER BY date DESC`, (err, rowNumber) => {
-            // if error on the query
-            if (err) {
-                res.json(responses.errorResponse('Error at query'));
-                return;
-            }
-
-            // if not, send everthing to the client
-            rowCount = rowNumber;
-        });
-
-        request.on('row', (data) => {
-            rows.push({
-                [data[0].metadata.colName]: data[0].value,
-                [data[1].metadata.colName]: String(data[1].value).trim(),
-                [data[2].metadata.colName]: data[2].value,
-                [data[3].metadata.colName]: Number(data[3].value),
+        if (req.params.id) {
+            // Search for it
+            const request = new Request(`SELECT id, description, completed, date FROM Todo WHERE id='${req.params.id}' ORDER BY date DESC`, (err, rowNumber) => {
+                // if error on the query
+                if (err) {
+                    res.json(responses.errorResponse('Error at query'));
+                    return;
+                }
+    
+                // if not, send everthing to the client
+                rowCount = rowNumber;
             });
-        });
+    
+            request.on('row', (data) => {
+                rows.push({
+                    [data[0].metadata.colName]: data[0].value,
+                    [data[1].metadata.colName]: String(data[1].value).trim(),
+                    [data[2].metadata.colName]: data[2].value,
+                    [data[3].metadata.colName]: Number(data[3].value),
+                });
+            });
+    
+            request.on('requestCompleted', () => {
+                res.json(responses.basicOkResponse({
+                    rows,
+                    rowCount,
+                }));
+            });
+    
+            // Execute the SQL
+            connection.execSql(request);
+        } else {
+            res.json(responses.errorResponse('No ID!'));
+        }
 
-        request.on('requestCompleted', () => {
-            res.json(responses.basicOkResponse({
-                rows,
-                rowCount,
-            }));
-        });
-
-        // Execute the SQL
-        connection.execSql(request);
     });
 
+    // Add a new todo
     app.post('/', (req, res) => {
         const { description, completed } = req.body;
         
@@ -136,9 +144,10 @@ connection.on('connect', (err) => {
             return;
         }
 
+        // date is always when you do the thing
         const date = Date.now();
 
-        const request = new Request(`INSERT INTO Todo (id, description, completed, value) VALUES ('${uuid.v1()}', '${description}', '${completed}', '${date}'`, (err) => {
+        const request = new Request(`INSERT INTO Todo (id, description, completed, date) VALUES ('${uuid.v1()}', '${description}', ${completed}, ${date})`, (err) => {
             if (err) {
                 res.json(responses.errorResponse(err));
                 return;
@@ -148,6 +157,54 @@ connection.on('connect', (err) => {
         });
 
         connection.execSql(request);
+    });
+
+    // Delete a todo
+    app.delete('/:id', (req, res) => {
+        if (req.params.id) {
+            const request = new Request(`DELETE FROM Todo WHERE id='${req.params.id}'`, (err) => {
+                if (err) {
+                    res.json(responses.errorResponse(err));
+                    return;
+                }
+    
+                res.json(responses.basicOkResponse());
+            });
+
+            connection.execSql(request);
+        } else {
+            res.json(responses.errorResponse('No ID!'));
+        }
+    });
+
+    // Update a todo
+    app.put('/:id', (req, res) => {
+        if (req.params.id) {
+            // decouple body
+            const { description, completed } = req.body;
+
+            // check if body has what is needed
+            if (description && completed) {
+                // date is always when you do the thing
+                const date = Date.now();
+
+                const request = new Request(`UPDATE Todo SET description = '${description}', completed = ${completed}, date = ${date} WHERE id = '${req.params.id}'`, (err) => {
+                    if (err) {
+                        res.json(responses.errorResponse(err));
+                        return;
+                    }
+        
+                    res.json(responses.basicOkResponse());
+                });
+    
+                connection.execSql(request);
+            } else {
+                res.json(responses.errorResponse('Bad body data'));
+                return;
+            }
+        } else {
+            res.json(responses.errorResponse('No ID!'));
+        }
     });
 
     app.listen(port, () => {
