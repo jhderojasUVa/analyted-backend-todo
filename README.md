35c0fb8b5edf0d58f78d7797e401e35f70afd3c4
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2022-08-12 11:33:30 +0100

    feat(): some end points working

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 # analyted-backend-todo
 
-Example of a Docker Node application that is an express server connecting to a database and exposing some endpoints for a Todo application
+Example of a Docker Node application that is an express server connecting to a database and exposing some endpoints for a Todo application.
+
+All responses are JSON based.
+
+# Endpoints
+
+## Any error
+
+```json
+{
+    "success": false,
+    "data": "The error message will be here"
+}
+```
+
+## GET /
+
+```json
+{
+    "success": true,
+    "data": {
+            "rows": [
+                {
+                    "id": "UNIQUE-ID",
+                    "description": "Description of the TODO",
+                    "completed": 0 || 1,
+                    "date": "UTC LONG FORMAT DATE"
+                }
+            ],
+            "rowCount": 0
+        }
+}
+```
+
+## GET /{id}
+
+```json
+{
+    "success": true,
+    "data": {
+            "rows": [],
+            "rowCount": 0
+        }
+}
+```
+
+## PUT /
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,135 @@
 // Simple express server
+const express = require('express');
+const { Connection, Request } = require('tedious');
+
+// responses
+const responses = require('./responses.js');
+
+// uid generator
+const uuid = require('uuid');
+const { response } = require('express');
+
+// express configuration
+const app = express();
+const port = 8080;
+// use JSON as put format
+app.use(express.json());
+
+// Tedious configuration and start up
+const config = {
+    server: 'localhost',
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'sa',
+            password: 'yourStrong(!)Password',
+        }
+    },
+    options: {
+        encrypt: true,
+        trustedConnection: true,
+        trustServerCertificate: true,
+        database: 'ToDos',
+    },
+};
+
+// connect to the database
+const connection = new Connection(config);
+
+// Trigger express only if connection is sucessfully
+connection.on('connect', (err) => {
+    if (err) {
+        console.log('There was an error connecting to the database');
+        console.log(err);
+        // Health check
+        app.get('/health', (req, res) => {
+            console.log('Hit: Health point');
+            res.json(responses.errorResponse('Unable to connect to databse'));
+        });
+    } else {
+        console.log('Connected to the database successfully!');
+        // Health check
+        app.get('/health', (req, res) => {
+            console.log('Hit: Health point');
+            res.json(responses.basicOkResponse);
+        });
+    }
+    
+    // Get all todos
+    app.get('/', (req, res) => {
+        const request = new Request('SELECT id, description, completed, date FROM Todo ORDER BY date DESC', (err, rowCount, rows) => {
+            // if error on the query
+            if (err) {
+                res.json(responses.errorResponse('Error at query'));
+            }
+
+            // if not, send everthing to the client
+            res.json(responses.basicOkResponse({
+                rows,
+                rowCount,
+            }));
+        });
+        
+        connection.execSql(request);
+    });
+
+    app.get('/:id', (req, res) => {
+        const request = new Request(`SELECT id, description, completed, date FROM Todo WHERE id="${req.params.id}" ORDER BY date DESC`, (err, rowCount, rows) => {
+            // if error on the query
+            if (err) {
+                res.json(responses.errorResponse('Error at query'));
+                return;
+            }
+
+            // if not, send everthing to the client
+            res.json(responses.basicOkResponse({
+                rows,
+                rowCount,
+            }));
+        });
+        
+        connection.execSql(request);
+    });
+
+    app.post('/', (req, res) => {
+        const { description, completed } = req.body;
+        
+        // Basic check
+        if (description === undefined || completed === undefined) {
+            res.json(responses.errorResponse('Error on JSON data'));
+            return;
+        }
+
+        const date = Date.now();
+
+        // const rrr = {
+        //     id: uuid.v1(),
+        //     description,
+        //     completed,
+        //     date,
+        // };
+
+        // console.log(rrr)
+
+        // res.json(responses.basicOkResponse({}));
+
+        const request = new Request(`INSERT INTO Todo (id, description, completed, value) VALUES ("${uuid.v1()}", "${description}", "${completed}", "${date}"`, (err) => {
+            if (err) {
+                res.json(responses.errorResponse(err));
+                return;
+            }
+
+            res.json(responses.basicOkResponse());
+        });
+
+        connection.execSql(request);
+    });
+
+    app.listen(port, () => {
+        console.log(`Express server running and listening to port ${port}`);
+    });
+});
+
+// Do the connection and trigger express server
+connection.connect();
+
