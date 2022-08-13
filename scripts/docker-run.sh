fce44f87351e1088ac7e989bc29d4e367e1494a3
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2022-08-13 09:26:34 +0100

    feat(docker): build/run/pull scripts

---
+++ scripts/docker-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Get version
+VERSION=$1
+
+# If you don't say the version it will always run the latest
+if [ -z "$1" ]
+then
+    docker run --name analyted-backend --network analyted_network -p 8080:8080 -d ghcr.io/analyted-backend:$1
+else
+    docker run --name analyted-backend --network analyted_network -p 8080:8080 -d ghcr.io/analyted-backend:latest
+fi
