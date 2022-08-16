a46ba2d1623b1b397dde63e1a35c75401d2fe0a2
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2022-08-16 11:30:41 +0100

    fix(): small docker fixes

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,7 +5,10 @@ VERSION=$1
 # If you don't say the version it will build the latest always
 if [ -z "$1" ]
 then
+    # Create both version and latest version
     docker build -t ghcr.io/jhderojasuva/analyted-backend-todo/analyted-backend:$1 .
+    docker build -t ghcr.io/jhderojasuva/analyted-backend-todo/analyted-backend:latest .
 else
+    # Create only latest version
     docker build -t ghcr.io/jhderojasuva/analyted-backend-todo/analyted-backend:latest .
 fi
